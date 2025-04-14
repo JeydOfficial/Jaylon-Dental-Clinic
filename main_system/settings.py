6842a8982f175cf2c13354844a7782cc524a87b8
JeydOfficial <dominicjadeofficial@gmail.com>
2025-04-14 11:39:41 +0800

    Fixes

--- main_system/settings.py
+++ main_system/settings.py
@@ -158,7 +158,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'jaylondentalclinic@gmail.com'
-EMAIL_HOST_PASSWORD = 'tclt wkiz mtgf hote'
+EMAIL_HOST_PASSWORD = 'ijzq bwnf arcj ucht'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
